0b8f02d1ca8d4fd0a04e3f63da7f38736a161d87
Rish7223 <rishabh.7223@gmail.com>
2021-05-15 19:42:06 +0530

    eslint & prettier setupt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-function App() {
-  return (
-    <div className="App">
-      Demo App
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">Demo App</div>;
+};
 
 export default App;
